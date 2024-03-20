8de2c57e15f58403010905451c9474ce5b08896b
edwardyq67 <edwardyq67@gmail.com>
2024-03-20 13:39:03 -0500

    nuaco

--- src/cliente/cliente.js
+++ src/cliente/cliente.js
@@ -8,8 +8,8 @@ try {
             <img class="h-auto max-w-full rounded-lg mx-auto" src="${res.data[i].url}" alt="">
             <div class="ver absolute w-[100%] h-[100%] top-0 bg-gradient-to-b from-transparent to-black transition-transform duration-20 ease-in-out transform translate-y-full group-hover:translate-y-0">
               <div class="contenido absolute inset-x-0 bg-transparent bottom-0 p-3 h-[auto]">
-                <h3 class="text-xl dark:text-white text-dark font-medium">${res.data[i].title}</h3>
-                <p class="overflow-y-auto h-[80%] leading-[1.3rem] dark:text-white text-dark text-[.8em]">${res.data[i].contend}</p>
+                <h3 class="text-xl text-white  font-medium">${res.data[i].title}</h3>
+                <p class="overflow-y-auto h-[80%] leading-[1.3rem] text-white text-[.8em]">${res.data[i].contend}</p>
               </div>
             </div>
           </div>`
@@ -32,8 +32,8 @@ const seleccion=(e)=>{
               <img class="h-auto max-w-full rounded-lg mx-auto" src="${res.data[i].url}" alt="">
               <div class="ver absolute w-[100%] h-[100%] top-0 bg-gradient-to-b from-transparent to-black transition-transform duration-20 ease-in-out transform translate-y-full group-hover:translate-y-0">
                 <div class="contenido absolute inset-x-0 bg-transparent bottom-0 p-3 h-[auto]">
-                  <h3 class="text-xl dark:text-white text-dark font-medium">${res.data[i].title}</h3>
-                  <p class="overflow-y-auto h-[80%] leading-[1.3rem] dark:text-white text-dark text-[.8em]">${res.data[i].contend}</p>
+                  <h3 class="text-xl text-white font-medium">${res.data[i].title}</h3>
+                  <p class="overflow-y-auto h-[80%] leading-[1.3rem] text-white text-[.8em]">${res.data[i].contend}</p>
                 </div>
               </div>
             </div>`
